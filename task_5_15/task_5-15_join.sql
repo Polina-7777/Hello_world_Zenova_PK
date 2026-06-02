d03424a49543f20c2768ef276fd7b89e835d76d1
Polina-7777 <pkzenova@mail.ru>
2026-06-02 18:09:40 +0300

    Delete task_5_15 directory

--- task_5_15/task_5-15_join.sql
+++
@@ -1,8 +0,0 @@
--- Выведите список цен товаров, используя алиасы таблиц: 
--- products → p, prices → pr. 
--- Вывод: "название товара" и "цена"
-SELECT 
-    p.name AS "Название товара", 
-    pr.price AS "Цена"
-FROM products p
-JOIN prices pr ON p.id = pr.product_id;
