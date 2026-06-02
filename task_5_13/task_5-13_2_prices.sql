086d18f0c4b412437e1dcd841b3f3bdb944284a4
Polina-7777 <pkzenova@mail.ru>
2026-06-02 18:09:17 +0300

    Delete task_5_13 directory

--- task_5_13/task_5-13_2_prices.sql
+++
@@ -1,6 +0,0 @@
--- Обновить цены товаров: увеличить на 5% для записей,
--- где product_id меньше или равен 5 и цена меньше 10000.
-UPDATE prices
-SET price = price * 1.05
-WHERE product_id <= 5 
-  AND price < 10000;
